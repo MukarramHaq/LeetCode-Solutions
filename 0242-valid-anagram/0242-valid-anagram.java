7437b934150fc55646e3dc8aacd570e0066abc01
MukarramHaq <44123470+MukarramHaq@users.noreply.github.com>
2023-09-17 14:10:43 -0400

    Time: 19 ms (16.71%), Space: 43.7 MB (38.33%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(t.length() != s.length()){
+            return false;
+        }
+        
+        HashMap<Character, Integer> myMap = new HashMap<>();
+        HashMap<Character, Integer> myMap2 = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            if(myMap.containsKey(t.charAt(i))){
+                myMap.put(t.charAt(i), myMap.get(t.charAt(i))+1);
+            }else{
+                myMap.put(t.charAt(i), 1);
+            }
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            if(myMap2.containsKey(s.charAt(i))){
+                myMap2.put(s.charAt(i), myMap2.get(s.charAt(i))+1);
+            }else{
+                myMap2.put(s.charAt(i), 1);
+            }
+        }
+
+        return myMap.equals(myMap2);
+    }
+}
