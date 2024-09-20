0d2fbd2dacceef649d1c2b4dd00b2e861cf94d8b
Mukarram Haq <mukarram796@gmail.com>
2024-09-19 21:09:59 -0400

    Update 0049-group-anagrams.java

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,13 +3,13 @@ class Solution {
         
         HashMap<String, List<String>> myMap = new HashMap<>();
         for(int i = 0; i < strs.length; i++){
-            char[] chArr = strs[i].toCharArray();
-            Arrays.sort(chArr);
-            String sortedWord = new String(chArr);
+            char[] arr = strs.[i].toCharArray();
+            Arrays.sort(arr);
+            String sortedWord = new String(arr);
             if(!myMap.containsKey(sortedWord)){
                 myMap.put(sortedWord, new ArrayList<>());
             }
-            myMap.get(sortedWord).add(strs[i]);
+            myMap.get(sortedWord).add(strs[i])
         }
 
         return new ArrayList<>(myMap.values());
